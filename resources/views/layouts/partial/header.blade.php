d0a20f7a426afccb80053cc00333a3749fd76413
sminth <emmanuelmalan225@gmail.com>
2023-02-16 13:46:10 +0000

    correction des burgs

--- resources/views/layouts/partial/header.blade.php
+++ resources/views/layouts/partial/header.blade.php
@@ -1,7 +1,7 @@
 <div class="site-header header-4 mb--20 d-none d-lg-block">
     @php
-        $numero = DB::table(['settings'])->where('key', 'contact_2')->first()->value;
-        $whatsapp = DB::table('settings')->where('key', 'contact_1')->first()->value;
-        $email = DB::table('settings')->where('key', 'email')->first()->value;
-        $facebook = DB::table('settings')->where('key', 'facebook_page')->first()->value;
+        // $numero = DB::table(['settings'])->where('key', 'contact_2')->first()->value;
+        // $whatsapp = DB::table('settings')->where('key', 'contact_1')->first()->value;
+        // $email = DB::table('settings')->where('key', 'email')->first()->value;
+        // $facebook = DB::table('settings')->where('key', 'facebook_page')->first()->value;
     @endphp
--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'index')->name('accueil');
+Route::get('/', 'HomeController@index')->name('accueil');
 
 
 
-/* route login et register
+/* route login et register */
 Route::get('/register', 'RegisterController@index')->name('register.index');
 Route::post('/traitement-register', 'RegisterController@traitement')->name('register.traitement');
 Route::get('/login', 'LoginController@index')->name('login.index');
@@ -31,11 +31,11 @@ Route::get('facebook/callback', 'LoginController@handleFacebookCallback');
 
 
 
-/* demande de restauration du mot de passe 
+/* demande de restauration du mot de passe */
 Route::get('/motdepasse-perdu', 'ForgetpassController@index')->name('forgot-password');
 Route::post('/demande-restauration', 'ForgetpassController@traitement')->name('request-password');
 Route::get('/confirm-reset/{token}', 'ForgetpassController@update')->name('confirm-request');
-/* pages statiques 
+/* pages statiques
 Route::view('/contact', 'static.contact')->name('contact');
 Route::view('/politiqueretour', 'static.politiqueretour')->name('politiqueretour');
 Route::view('/apropos', 'static.apropos')->name('apropos');
@@ -51,25 +51,25 @@ Route::group([
     // Route::get('/mes-commandes', 'ClientController@commande')->name('client.commande');
     // Route::get('/commande/details/{commande}', 'ClientController@detail_commande')->name('client.commande.details');
 
-    /* mot de passe 
+    /* mot de passe
     Route::get('/changer-motdepasse', 'ClientController@password')->name('client.motdepasse');
     Route::post('/update-password', 'ClientController@updatepassword')->name('client.updatepassword');
 
-    /* information personnelle 
+    /* information personnelle
     Route::get('/info', 'ClientController@info')->name('client.info');
     Route::post('/update-info', 'ClientController@updateinfo')->name('client.updateinfo');
 
-    /* adresse 
+    /* adresse
     Route::get('/adresse', 'ClientController@adresse')->name('client.adresse');
     Route::post('/update-adresse', 'ClientController@updateadresse')->name('client.updateadresse');
 
-    /* deconnexion 
+    /* deconnexion
     Route::get('/deconnexion', 'ClientController@deconnexion')->name('client.logout');
 
 
 });
 
-/* pour la page vuejs 
+/* pour la page vuejs
 Route::group([
     'prefix' => config('backpack.base.route_prefix', 'admin'),
     'middleware' => array_merge(
@@ -77,7 +77,7 @@ Route::group([
         (array)config('backpack.base.middleware_key', 'admin')
     )
 ], function () { // custom admin routes
-    /* pour vuejs 
+    /* pour vuejs
     Route::view('/v/{vue_capture?}', 'vue.index')->where('vue_capture', '[\/\w\.-]*');
 });
 */
